2f27f8d58174f05948cf268750d04609dde4b6a4
qawksl <lerochka.sayfullina@mail.ru>
2024-09-17 09:34:28 +0500

    add ggg1

---
+++ ggg1/ggg/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ggg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // О(N)
+            long fact = 1;
+            int h1 = Convert.ToInt32(Console.ReadLine()); // Факториал какого числа.
+            if (fact == 0)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                for (int i = 1; i <= h1; i++)
+                {
+                    fact *= i;
+                }
+                Console.WriteLine(fact);
+            }
+        }
+    }
+}
